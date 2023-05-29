2f78a1bea47cd13f3b902da61816d06da90350f5
Ian Machado <109290144+Iannmachado@users.noreply.github.com>
2023-05-29 14:43:42 -0300

    Create Nodo.java

---
+++ src/juego/Nodo.java
@@ -0,0 +1,15 @@
+package juego;
+
+public class Nodo {
+	Meteorito meteorito;
+	Nodo siguiente;
+
+
+    // Constructor
+    public Nodo(Meteorito meteorito) {
+        this.meteorito = meteorito;
+        this.siguiente = null;
+
+    }
+
+}
