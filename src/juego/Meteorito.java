24606df25160dc0931d78e64453f240de8933851
Ian Machado <109290144+Iannmachado@users.noreply.github.com>
2023-05-29 14:40:25 -0300

    Update Meteorito.java

--- src/juego/Meteorito.java
+++ src/juego/Meteorito.java
@@ -2,88 +2,91 @@ package juego;
 
 import java.awt.Color;
 import java.awt.Image;
-import java.util.Random;
+
 import entorno.Entorno;
 import entorno.Herramientas;
 
-
-public class Meteorito {
-
-	// Variables
-	double x;
-	double y;
-	int ancho;
-	int alto;
-	double angulo;
-	boolean exploto;
-	Image img;
-	
-	Entorno entorno;
+public class Nave 
+{
+	// Variables de instancia
+	boolean disparando;
+	private Entorno entorno;
 	Bala municion;
-	Nave nave;
-	
-	
-	public Meteorito(double x, double y) {
-		this.x = x;
-		this.y = y;
-		this.ancho = 30;
-		this.alto = 30;
-		this.exploto = false;
-		
-		img = Herramientas.cargarImagen("Meteorito.png");
-		
-	}
-	
-	public void girar() {
-		this.angulo += 0.01;
-	}
-	
-	Random random = new Random();
-    double randomNumber = random.nextInt(2);
-    
-	
-	public void caer(double x, double y) {
-		
-        this.y += Math.sin(2)*1;
-        
-        if(randomNumber == 1) {
-        	this.x += Math.cos(this.angulo)*0.2;
-        } else {
-        	this.x -= Math.cos(this.angulo)*0.2;
+	private int	entornoAncho;
+	private double naveX;
+	private double naveY;
+	public double naveAncho;
+	private double angulo;
+	private Image  img1;
+	//private Image img2;
+	//private int velocidadDisparo = 10;
 
-        }
-		
-		if(this.y >= 650 || this.x >= 850 || this.x <= -10) {
-			this.x = (int) (1400 - 1800*Math.random());
-			this.y = 0;
-		}	
+	public Nave(Entorno entorno) 
+	{
+		this.disparando = false;
+		this.entorno=entorno;
+		this.entornoAncho=entorno.ancho();
+		this.angulo=-1.5708;
+		this.naveX = 400;
+		this.naveY = 560;
+		this.img1 = Herramientas.cargarImagen("nave.png");
+		this.naveAncho = 30;
+		//img2 = Herramientas.cargarImagen("navee.png");
 		
 	}
+	public double naveGetX() {
+		return this.naveX;
+	}
 	
-	public void circulo(Entorno entorno) {
-		//entorno.dibujarCirculo(this.x, this.y, 34, Color.cyan);
-		entorno.dibujarRectangulo(this.x, this.y, 30, 30, this.angulo, Color.pink);
+	public double naveGetY() {
+		return this.naveY;
+	}
+	
+	public void dibujarse(Entorno entorno)
+	{
+		entorno.dibujarTriangulo(this.naveX, this.naveY, 50, 30, this.angulo, Color.yellow);
+		entorno.dibujarImagen(img1, this.naveX, this.naveY, 0, 0.2);	
+	}
+	
+	
+	public void moverDerecha() /* Mueve a nave a la derecha sin pasar del ancho de la ventana */
+	{
+		if(this.naveX+5+(this.naveAncho/2) <= this.entornoAncho)
+			this.naveX=this.naveX+3;
+			//System.out.println("derecha");
+	}
 
+	public void moverIzquierda() // Mueve a nave a la izquierda sin pasar del ancho de la ventana
+	{
+		if(this.naveX-5-(this.naveAncho/2) >= 0)
+			this.naveX=this.naveX-3;
+			//System.out.println("izquierda");
 	}
 	
-	public void dibujarse(Entorno entorno){
-		//entorno.dibujarCirculo(this.x, this.y, 34, Color.black);
-		if(!this.exploto) {
-			this.circulo(entorno);
-			entorno.dibujarImagen(img, this.x, this.y, this.angulo, 0.1);
-			this.girar();
-			this.caer(this.x, this.y);	
-		} else {
-			entorno.dibujarImagen(img, this.x, this.y, 0, 0);
-			
+	public void disparar () {
+		if(!disparando) {
+			this.disparando=true;
+			this.municion=new Bala(this.naveGetX(),this.naveGetY()-60,30,50,3);
 		}
 	}
 	
-	public void exploto() {
-		img = Herramientas.cargarImagen("Meteorito-Explosion.png");
-		this.exploto = true;
+	public void moverDisparo() {
+		if(disparando && enPantalla()) {
+			this.municion.setY(10);
+			this.municion.redibujar(this.entorno);
+		}
 	}
 	
+	public boolean enPantalla () {
+		if(municion.getY()+20<0) {
+			borrarMunicion();
+			return false;
+		}else return true;
+	}
 	
-	
+	public void borrarMunicion() {
+		this.disparando=false;
+		this.municion=null;
+		System.out.println("seb orro la municion");
+	}
 }
